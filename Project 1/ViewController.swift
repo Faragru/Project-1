0022148a00dadaa55574ecd1c6a94848a7d69304
Faragru <ciprians39@yahoo.com>
2023-05-27 20:00:15 +0300

    Initial Commit

---
+++ Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Stan Ciprian on 27.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
